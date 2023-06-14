1983b33f2bf4cc1aad29a852920a10ec623847bd
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-06-14 10:16:25 +0500

    Add project files.

---
+++ KafeWebSite/Controllers/KitchenController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KafeWebSite.Controllers
+{
+    public class KitchenController : Controller
+    {
+        private readonly ILogger<KitchenController> _logger;
+
+        public KitchenController(ILogger<KitchenController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult News_detail()
+        {
+            return View();
+        }
+    }
+}
